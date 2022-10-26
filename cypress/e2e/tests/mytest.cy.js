af9a074413f14640a2d2b7dece3d09e3f7886818
dymafr <contact@dyma.fr>
2022-10-26 15:27:54 +0200

    l4

--- cypress/e2e/tests/mytest.cy.js
+++ cypress/e2e/tests/mytest.cy.js
@@ -3,6 +3,11 @@ describe('my first set of tests', () => {
     cy.visit('/');
     cy.contains('h1', /list/);
     cy.findByText(/todo/i);
-    cy.get('.container').find('h1');
+    cy.get('.container')
+      .find('h1')
+      .should('be.a', 'object')
+      .and('have.class', 'mb-20')
+      .and('have.css', 'background-color', 'rgb(255, 0, 0)')
+      .and('have.attr', 'id', 'unid');
   });
 });
