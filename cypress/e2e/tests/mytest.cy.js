1e7bee5dea68042cc1e26a94884128c26bc75f51
dymafr <contact@dyma.fr>
2022-10-26 18:00:49 +0200

    l7

--- cypress/e2e/tests/mytest.cy.js
+++ cypress/e2e/tests/mytest.cy.js
@@ -1,45 +1,20 @@
 /// <reference types="cypress" />
 
+import todos from '../../fixtures/todos';
+
+const BASE_URL = 'https://restapi.fr/api/rtodo';
+
 describe('my first set of tests', () => {
   before(() => {
+    cy.intercept(BASE_URL, { fixture: 'todos' }).as('fetchedTodos');
+    cy.fixture('todos').as('todos');
     cy.visit('/');
   });
 
-  //   it('my first test', () => {
-  //     cy.contains('h1', /list/);
-  //     cy.findByText(/todo/i);
-  //     cy.get('.container')
-  //       .find('h1')
-  //       .should('be.a', 'object')
-  //       .and('have.class', 'mb-20')
-  //       .and('have.css', 'background-color', 'rgb(255, 0, 0)')
-  //       .and('have.attr', 'id', 'unid');
+  it('my first test', () => {
+    cy.wait('@fetchedTodos').its('request.method').should('equal', 'GET');
 
-  //     cy.get('input').should('exist').get('h1').should('be.a', 'object');
-  //   });
-
-  //   it('my second test', () => {
-  //     //     cy.get('input')
-  //     //       .type('manger une pizza', { delay: 50 })
-  //     //       .get('button')
-  //     //       .first()
-  //     //       .click();
-
-  //     cy.wrap({ name: 'jean' }).its('name').should('equal', 'jean');
-  //     cy.wrap({ name: () => 'jean' })
-  //       .invoke('name')
-  //       .should('eq', 'jean');
-
-  //     const input = cy.get('input');
-  //     input.wait(1000);
-  //     input.clear();
-  //   });
-
-  it('my third test', () => {
-    const inputs = cy.get('input');
-    const checkbox = inputs.filter('[type="checkbox"]');
-    checkbox.check().wait(500).uncheck();
-
-    cy.get('select').select('2').wait(1000).select('3');
+    cy.get('@todos').should('have.length', 4);
+    cy.wrap(todos).should('have.length', 4);
   });
 });
